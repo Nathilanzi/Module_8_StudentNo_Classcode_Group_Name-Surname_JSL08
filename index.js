7df9b6f6c18c921c1b002f9015f5b71eacba4a97
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-04-11 09:16:50 +0200

    Fixed CSS

--- index.js
+++ index.js
@@ -32,3 +32,31 @@
 // - The `getBranchInfo` method allows us to retrieve the branch information from the singleton instance.
 // - We demonstrate the Singleton pattern by creating two instances (`branchA` and `branchB`) and show that they both point to the same instance,
 //   ensuring that there is only one set of branch information throughout the application.
+//BankBranch class
+class BankBranch {
+    constructor(branchInfo) {
+        if (!BankBranch.instance) {
+            this.branchInfo = "null";
+            BankBranch.instance = this;
+        }
+        return BankBranch.instance;
+    }
+
+    getBranchInfo() {
+        return this.branchInfo;
+    }
+}
+
+// Variables to store the singleton instance of the main bank branch
+const bankBranchInstance = new BankBranch({ name: "Main Branch", location: "City Center" });
+
+// Instances of the  class "BankBranch" with different branch information
+const branchA = new BankBranch({ name: "Branch A", location: "Polokwane" });  //"Branch A" is located in "Polokwane"
+const branchB = new BankBranch({ name: "Branch B", location: "Durban" }); //"Branch B" is located in "Durban"
+
+// Retrieving branch information from the instances using "getBranchInfo", output for both should be { name: "Main Branch", location: "City Center" }
+console.log(branchA.getBranchInfo()); 
+console.log(branchB.getBranchInfo());  
+
+// Verifying that `branchA` and `branchB` are both referring to the same instance using "===" (truthy/falsy)
+console.log(branchA === branchB); // Output: true
